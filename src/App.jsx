18b58739e8c9c6376cde08b6e6d87f3d87ca5fca
newhouse111 <edusupercasanova@gmail.com>
2024-10-29 22:21:12 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+import SectionA from './SectionA'
+
+function App() {
+
+
+  return (
+    <>
+      <SectionA/>
+    </>
+  )
+}
+
+export default App
